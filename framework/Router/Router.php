cfab12334a87d49b526b90a8d43b5979aff460e1
Vladimir Gershenko <gera8469@gmail.com>
2015-12-28 10:41:13 +0200

    feature(router): developing

--- framework/Router/Router.php
+++ framework/Router/Router.php
@@ -4,9 +4,13 @@ namespace Framework\Router;
 
 class Router
 {
+    private static $controller;
+
+    private static  $action;
+
     public static function getRoute()
     {
-        $uri =  $_SERVER['REQUEST_URI'];
+        $uri =  urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
         $routes = require_once(__DIR__.'/../../app/config/routes.php');
         echo '<pre>';
