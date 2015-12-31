7052940e209d102d20ac44c1c1e6745e301d9e1e
GerashenkoVladimir <gera8469@gmail.com>
2015-12-31 17:15:37 +0200

    fix(Router.php): fixed "if" blocks

--- framework/Router/Router.php
+++ framework/Router/Router.php
@@ -4,7 +4,7 @@ namespace Framework\Router;
 
 class Router
 {
-    private static $matchedRoutes = array();
+    private static $readyRoute = array();
 
     private static $controller;
 
@@ -18,36 +18,66 @@ class Router
 
         $routes = require_once(__DIR__.'/../../app/config/routes.php');
 
-        foreach ($routes as $route) {
-            $pattern = $route['pattern'];
-            if (preg_match_all('|{(\w+)}|', $route['pattern'], $matches) != null) {
-                foreach ($matches[1] as $match => $m) {
-                    $pattern = preg_replace('|'.$matches[0][$match].'|', '('.$route['_requirements'][$m].')', $pattern);
-                }
-            }
+        $matchedRoutes = array();
 
-            if (preg_match("|^".$pattern.'$|', $uri)) {
-                array_push(self::$matchedRoutes, $route);
-                $args = explode('/', $uri);
-                array_shift($args);
-                array_shift($args);
-                self::$args = $args;
-                if (count(self::$matchedRoutes) > 0) {
-                    foreach (self::$matchedRoutes as $mR) {
-                        if (isset($mR['_requirements']['_method']) && $_SERVER['REQUEST_METHOD'] == $mR['_requirements']['_method']) {
-                            self::$matchedRoutes[0] = $mR;
-                        }
-                    }
-                }
+        foreach ($routes as $route) {
+            $pattern = self::preparePattern($route);
+
+            if (self::compareRoute($pattern, $uri)) {
+                array_push($matchedRoutes, $route);
             }
         }
 
-        if (self::$matchedRoutes != null) {
-            self::$controller = self::$matchedRoutes[0]['controller'];
-            self::$action     = self::$matchedRoutes[0]['action'].'Action';
+        self::$readyRoute = self::filterHTTPMethods($matchedRoutes);
+        self::$args = self::getArgs($uri);
+
+        if (self::$readyRoute != null) {
+            self::$controller = self::$readyRoute['controller'];
+            self::$action     = self::$readyRoute['action'].'Action';
             call_user_func_array(array(new self::$controller, self::$action), self::$args);
         } else {
             echo '404';
         }
     }
+
+    private static function preparePattern($route)
+    {
+        $pattern = $route['pattern'];
+        if (preg_match_all('|{(\w+)}|', $route['pattern'], $matches) != null) {
+            foreach ($matches[1] as $match => $m) {
+                $pattern = preg_replace('|'.$matches[0][$match].'|', '('.$route['_requirements'][$m].')', $pattern);
+            }
+        }
+        return $pattern;
+    }
+
+    private static function compareRoute($pattern, $uri)
+    {
+        if (preg_match("|^".$pattern.'$|', $uri)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static function getArgs($uri)
+    {
+        $args = explode('/', $uri);
+        array_shift($args);
+        array_shift($args);
+        return $args;
+    }
+
+    private static function filterHTTPMethods($matchedRoutes)
+    {
+        if (count($matchedRoutes) > 0) {
+            foreach ($matchedRoutes as $mR) {
+                if (isset($mR['_requirements']['_method']) && $_SERVER['REQUEST_METHOD'] == $mR['_requirements']['_method']) {
+                    $filteredRoute = $mR;
+                    return $filteredRoute;
+                }
+            }
+        }
+        return $matchedRoutes[0];
+    }
+
 }
